bdd9d2e76ad774ea58407a261a6597c7b47cc1eb
JustJimer <46703655+JustJimer@users.noreply.github.com>
2024-04-23 21:46:51 +0300

    lr6 solution

---
+++ LR6_Chupyna_ASP_402/LR6_Chupyna_ASP_402/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace LR6_Chupyna_ASP_402
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ LR6_Chupyna_ASP_402/LR6_Chupyna_ASP_402/User.cs
@@ -0,0 +1,8 @@
+﻿namespace LR6_Chupyna_ASP_402
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
