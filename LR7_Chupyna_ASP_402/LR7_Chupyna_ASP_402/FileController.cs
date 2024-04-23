a213ada543601462aee27d815ec5852aa09bb6c1
JustJimer <46703655+JustJimer@users.noreply.github.com>
2024-04-23 22:32:32 +0300

    lr7 solution

---
+++ LR7_Chupyna_ASP_402/LR7_Chupyna_ASP_402/FileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace LR7_Chupyna_ASP_402.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet("File/DownloadFile")]
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost("File/DownloadFile")]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            string content = $"Name:\t {firstName}\nSurname:\t {lastName}";
+            byte[] byteArray = Encoding.UTF8.GetBytes(content);
+
+            return File(byteArray, "text/plain", fileName + ".txt");
+        }
+    }
+}
