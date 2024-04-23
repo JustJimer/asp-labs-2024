ce427ec37b9aca8d86aeba0f3ad2c2ec04f4f96c
JustJimer <46703655+JustJimer@users.noreply.github.com>
2024-04-23 23:24:46 +0300

    lr8 solution

---
+++ LR8_Chupyna_ASP_402/LR8_Chupyna_ASP_402/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LR8_Chupyna_ASP_402
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { ID = 1, Name = "lastMinuteLabs", Price = 10.99m, CreatedDate = DateTime.Now.AddDays(-5) },
+                new Product { ID = 2, Name = "pizza orders here", Price = 20.99m, CreatedDate = DateTime.Now.AddDays(-3) },
+                new Product { ID = 3, Name = "something else here", Price = 15.99m, CreatedDate = DateTime.Now.AddDays(-1) }
+            };
+
+            return View(products);
+        }
+    }
+}
