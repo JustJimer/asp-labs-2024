fca1fe7d72d4b8029ad8223be3e6a1e0065570c2
JustJimer <46703655+JustJimer@users.noreply.github.com>
2024-04-23 19:15:07 +0300

    lr5 solution

---
+++ LR5_Chupyna_ASP_402/LR5_Chupyna_ASP_402/Program.cs
@@ -0,0 +1,17 @@
+namespace LR5_Chupyna_ASP_402
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ LR5_Chupyna_ASP_402/LR5_Chupyna_ASP_402/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace LR5_Chupyna_ASP_402
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<ExceptionLoggingMiddleware>();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
