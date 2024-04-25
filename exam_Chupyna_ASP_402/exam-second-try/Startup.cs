252e822750786facc56a4496145d4a59285b204e
JustJimer <46703655+JustJimer@users.noreply.github.com>
2024-04-25 12:41:49 +0300

    email tag hepler

---
+++ exam_Chupyna_ASP_402/exam-second-try/Startup.cs
@@ -0,0 +1,23 @@
+﻿namespace exam_Chupyna_ASP_402
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+    }
+}
---
+++ exam_Chupyna_ASP_402/exam-second-try/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace exam_second_try.TagHelpers
+{
+    [HtmlTargetElement("email")]
+    public class EmailTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", $"mailto:{output.Content.GetContent()}");
+            output.Content.SetContent(output.Content.GetContent());
+        }
+    }
+}
